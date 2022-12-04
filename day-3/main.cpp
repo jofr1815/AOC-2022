641d262afd78b2beabd6c984e4dfbe2d53ee5e34
jofr1815 <75509140+jofr1815@users.noreply.github.com>
2022-12-03 19:49:40 -0700

    Added functionality to parse input data into lines, added helper function to convert char to priority score

--- day-3/main.cpp
+++ day-3/main.cpp
@@ -4,6 +4,23 @@
 
 using namespace std;
 
+int charToPriority (char c) {
+    if (c >= 97 && c <= 122) {
+        return c - 96;
+    } else if (c >= 65) {
+        return c - 38;
+    }
+    return -1;
+}
+
 int main() {
-    reutnr 0;
+    string line;
+    ifstream input ("input.txt");
+    string left;
+    string right;
+    while (!input.eof()) {
+        getline(input, line);
+        cout << line << endl << line.length() << endl;
+    }
+    cout << charToPriority('A') << endl;
 }
