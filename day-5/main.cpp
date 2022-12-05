62c7abc689ff3a190ba8a247f14095a3138abd99
Cam Frederickson <camfrederickson@cu-engr2-1-13-10.201.49.94.int.colorado.edu>
2022-12-05 11:21:06 -0700

    Added segment to parse the box data into an array of strings

--- day-5/main.cpp
+++ day-5/main.cpp
@@ -5,5 +5,31 @@
 using namespace std;
 
 int main() {
+    // Variables for parsing input into lines
+    fstream input;
+    string line;
+
+    // Array of strings to hold the stack of boxes
+    string arr[9];
+
+    // Open file and get first line
+    input.open("input.txt");
+    getline(input, line);
+    while(line.length() != 0 && line[1] != '1') {
+        for (int i = 0; i < line.length(); i++) {
+            cout << line[i*4 + 1] << " ";
+            if (line[i*4 + 1] != ' ' && line[i*4 + 1] != 0) {
+                arr[i].push_back(line[i*4 + 1]);
+            }
+        }
+        cout << endl;
+        getline(input, line);
+    }
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < arr[i].length(); j++) {
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
     return 0;
 }
