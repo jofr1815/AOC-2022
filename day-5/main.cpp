136a41b2f67e24f6d0a089a5179dc7cfd3bc056b
Cam Frederickson <camfrederickson@muen1-2-143-dhcp.int.colorado.edu>
2022-12-05 16:18:31 -0700

    Added parsing of the instructions

--- day-5/main.cpp
+++ day-5/main.cpp
@@ -45,9 +45,57 @@ int main() {
     // Print out array of strings
     printArr(arr);
 
-    string x = "ABC";
-    reverseStr(x);
-    cout << x << endl;
+    // Reverse strings so that top box is at the top of the stack
+    for (int i = 0; i < 9; i ++) {
+        reverseStr(arr[i]);
+    }
+    // Print out array to verify the strings have been reversed
+    printArr(arr);
 
+    // Create and initialize variables for parsing instructions
+    int quantity = 0;
+    int origin = 0;
+    int dest = 0;
+    int index = 0;
+    string parser;
+
+    // Print out rest of file, ie the instructions
+    while (!input.eof()) {
+        getline(input, line);
+        cout << line << endl;
+        // Parse instruction into quantity, origin, and destination
+        index = 0;
+
+        // Find the start of the quantity #
+        while(line[index] < 48 || line[index] > 57) {
+            index++;
+        }
+        // Insert number into a parsing string
+        while(line[index] != ' ') {
+            parser.push_back(line[index]);
+            index++;
+        }
+
+        // Set quantity to the value extracted by the parser
+        quantity = stoi(parser);
+        cout << quantity << " ";
+
+        // Clear the parser for the next number
+        parser.clear();
+
+        // Continue parsing the string to the next number (origin)
+        while(line[index] < 48 || line[index] > 57) {
+            index++;
+        }
+
+        // Extract the second number from the input string
+        // The locations are labeled 1 through 9, so now we can just extract the character at the index
+        origin = int(line[index] - '0');
+        cout << origin << " ";
+
+        // Extract the end number (destination)
+        dest = int(line[line.length() - 1] - '0');
+        cout << dest << endl;
+    }
     return 0;
 }
