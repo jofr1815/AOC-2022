de3f2b89efd4238ee634897f423ec2b0b5ffd60b
Cam Frederickson <camfrederickson@cu-engr2-1-13-10.201.49.94.int.colorado.edu>
2022-12-05 09:40:20 -0700

    Completed part 1 of day 4

--- day-4/main.cpp
+++ day-4/main.cpp
@@ -4,5 +4,54 @@
 using namespace std;
 
 int main() {
-    return 0;
+    ifstream input;
+    input.open("input.txt");
+    string line;
+    string leftLower;
+    string leftUpper;
+    string rightLower;
+    string rightUpper;
+    int index = 0;
+    int total = 0;
+    while (!input.eof()) {
+        getline(input, line);
+        cout << line << endl;
+        // Extract first number
+        while(line[index] != '-'){
+            leftLower.push_back(line[index]);
+            index++;
+        }
+        index++;
+        // Extract second number
+        while(line[index] != ','){
+            leftUpper.push_back(line[index]);
+            index++;
+        }
+        index++;
+        // Extract third number
+        while(line[index] != '-'){
+            rightLower.push_back(line[index]);
+            index++;
+        }
+        index++;
+        // Extract fourth number
+        while(index < line.length()){
+            rightUpper.push_back(line[index]);
+            index++;
+        }
+        cout << leftLower << " " << leftUpper << endl;
+        cout << rightLower << " " << rightUpper << endl;
+
+        // If one range completely includes the other, increment total
+        if((stoi(leftLower) >= stoi(rightLower) && stoi(leftUpper) <= stoi(rightUpper)) || (stoi(leftLower) <= stoi(rightLower) && stoi(leftUpper) >= stoi(rightUpper))) {
+            cout << "One range holds the other" << endl;
+            total++;
+        }
+        index = 0;
+        leftLower = "";
+        leftUpper = "";
+        rightLower = "";
+        rightUpper = "";
+    }
+    cout << total << endl;
 }
