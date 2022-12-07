3116c040d3ebe7e58a6b00fe0b68bbe82dda41ac
jofr1815 <jofr1815@colorado.edu>
2022-12-07 13:58:49 -0700

    Started building data structure for directories

--- day-7/main.cpp
+++ day-7/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+struct directory {
+    directory* parent;
+    vector<directory*> subDirs;
+    string name;
+    int size = 0;
+};
+
 int main() {
+    directory* home = new directory;
+    home->name = "/";
+    cout << home->size << endl;
+    cout << home->name << endl;
+    directory* newDir = new directory;
+    newDir->name = "/a";
+    home->subDirs.push_back(newDir);
+    cout << home->subDirs[0]->name << endl;
     return 0;
 }
