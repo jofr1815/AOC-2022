677d073c4f0afc88119e6c6cc5909861b5973846
Cam Frederickson <camfrederickson@econ2-207-172-dhcp.int.colorado.edu>
2022-12-07 10:03:04 -0700

    Completed part 1 of day 6

--- day-6/main.cpp
+++ day-6/main.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+bool compareHelper(char a, char b, char c, char d) {
+    if (a == b || a == c || a == d || b == c || b == d || c == d) {
+        return false;
+    }
+    return true;
+}
+
+int findStart(string input) {
+    for (int i = 3; i < input.length(); i++) {
+        if (compareHelper(input[i], input[i-1], input[i-2], input[i-3])) {
+            return i + 1;
+        }
+    }
+    return 0;
+}
+
 int main() {
+
+    fstream input;
+    string line;
+    input.open("input.txt");
+    getline(input, line);
+    cout << findStart(line) << endl;
+    input.close();
     return 0;
 }
